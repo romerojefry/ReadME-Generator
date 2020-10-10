66055f5f472768092c290d19dd290ad41469ae4d
Jefry Romero <romerojefry@icloud.com>
2020-10-09 17:20:57 -0700

    nmp install

---
+++ homework/Develop/index.js
@@ -0,0 +1,22 @@
+const inquier = require("inquirer");
+const fs = require("fs");
+//const axios = re('axios');
+
+const questions = [{
+    // type: "input",
+    // name:"title",
+    //message: "whats is your project title?"
+};
+{
+    
+}
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
