45f20cfea2cc8d450afb421d678161fefa011eee
Jefry Romero <romerojefry@icloud.com>
2020-10-09 18:01:30 -0700

    adding practice prompts

--- homework/Develop/index.js
+++ homework/Develop/index.js
@@ -1,22 +1,23 @@
-const inquier = require("inquirer");
-const fs = require("fs");
-//const axios = re('axios');
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-const questions = [{
-    // type: "input",
-    // name:"title",
-    //message: "whats is your project title?"
-};
+inquirer.prompt({
+    type: "input",
+    message: "Wat is your Github username?",
+    name: "username",
+},
 {
-    
-}
-];
-
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
+    type: "input",
+    message: "What is your Github email?",
+    name: "email"
+},
+{
+    type: "input",
+    message: "what is your project name",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What kind of license should your project have?",
+    name: "license"
+})
